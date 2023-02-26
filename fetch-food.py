fdc721d4a8a3fb93454a6f82a43c09423a45b695
siykt <cnsiykt@163.com>
2023-02-27 00:09:45 +0800

    feat: FoodMeiShiChina

--- fetch-food.py
+++ fetch-food.py
@@ -3,12 +3,18 @@
 # -------------------------------------------
 # 菜谱
 from pathlib import Path
-from utils.req import downloadPDF, getHTMLDom
+from utils.req import getHTMLDom
+import requests
 
 # 下载目录
 DOWNLOAD_DIR = Path('.') / 'data' / 'foods'
 # 线程上限
 MAX_THREAD_NUM = 24
+REQUEST_FAKER_HEADER = {
+    'Content-Type': 'application/json;charset=UTF-8',
+    'accept': 'application/json, text/plain, */*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+}
 
 
 class Food2345():
@@ -53,10 +59,56 @@ class Food2345():
             pass
 
 
+class FoodMeiShiChina():
+    FOOD_MEI_SHI_CHINA_WEBSITE_TYPE_DICT = [
+        # 最新推荐
+        'hot',
+        # 最新发布
+        'new',
+        # 热菜
+        102,
+        # 凉菜
+        202,
+        # 汤羹
+        57,
+        # 主食
+        59,
+        # 小吃
+        62,
+        # 西餐
+        160,
+        # 烘培
+        60,
+        # 自制
+        69,
+    ]
+    FETCH_URL = 'https://home.meishichina.com/ajax/ajax.php?ac=recipe&op=getMoreDiffStateRecipeList&'
+
+    def __init__(self, foodType, page=1):
+        self.orderby = 'tag' if type(foodType) == int else foodType
+        self.classid = foodType if type(foodType) == int else 0
+        self.page = page
+
+    def __fetch(self):
+        return requests.get(f'{self.FETCH_URL}classid={self.classid}&orderby={self.orderby}', headers=REQUEST_FAKER_HEADER)
+
+    def fetch(self):
+        return self.__fetch()
+
+    def nextPage(self):
+        try:
+            self.page += 1
+            return requests.get(f'{self.FETCH_URL}classid={self.classid}&orderby={self.orderby}&page={self.page}').json()
+        except:
+            self.page -= 1
+
+
 if __name__ == '__main__':
-    f = Food2345(Food2345.FOOD_2345_WEBSITE_TYPE_DICT[0][0], Food2345.FOOD_2345_WEBSITE_TYPE_DICT[0][1])
-    res = f.fetch()
-    if res:
-        for title, src in zip(res[0], res[2]):
-            print('标题:', title)
-            print('详情:', src)
+    # f = Food2345(Food2345.FOOD_2345_WEBSITE_TYPE_DICT[0][0], Food2345.FOOD_2345_WEBSITE_TYPE_DICT[0][1])
+    # res = f.fetch()
+    # if res:
+    #     for title, src in zip(res[0], res[2]):
+    #         print('标题:', title)
+    #         print('详情:', src)
+    res = FoodMeiShiChina(FoodMeiShiChina.FOOD_MEI_SHI_CHINA_WEBSITE_TYPE_DICT[0]).fetch()
+    print('res ->', res.json())
