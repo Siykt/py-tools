31349b9f75e4f37606f44b4a87bc8ce3ea2372c1
siykt <cnsiykt@163.com>
2023-02-26 23:47:57 +0800

    refactor: use Food2345

--- fetch-food.py
+++ fetch-food.py
@@ -9,22 +9,34 @@ from utils.req import downloadPDF, getHTMLDom
 DOWNLOAD_DIR = Path('.') / 'data' / 'foods'
 # 线程上限
 MAX_THREAD_NUM = 24
-FETCH_URL = 'http://tools.2345.com'
-BASE_GET_LIST_URL = 'http://tools.2345.com/frame/menu/getList?atype=ca&cate='
 
 
-class Food():
+class Food2345():
+    FOOD_2345_WEBSITE_TYPE_DICT = [
+        # 早餐
+        ('http://tools.2345.com/meishi/zaocan.htm', 62),
+        # 午餐
+        ('http://tools.2345.com/meishi/wucan.htm', 63),
+        # 晚餐
+        ('http://tools.2345.com/meishi/wancan.htm', 64),
+        # 夜宵
+        ('http://tools.2345.com/meishi/xiaoye.htm', 65),
+        # 下午茶
+        ('http://tools.2345.com/meishi/xiawucha.htm', 66),
+    ]
+    FETCH_URL = 'http://tools.2345.com'
+    BASE_GET_LIST_URL = 'http://tools.2345.com/frame/menu/getList?atype=ca&cate='
 
     def __init__(self, url: str, cate: int, page=1) -> None:
         self.url = url
-        self.listUrl = f'{BASE_GET_LIST_URL}{cate}'
+        self.listUrl = f'{self.BASE_GET_LIST_URL}{cate}'
         self.page = page
 
     def __fetch(self, url):
         soup = getHTMLDom(url, 'gbk')
         titles = [x.text.strip() for x in soup.select('.img-mod-wrapper .img-title-txt')]
-        imgs = [f"{FETCH_URL}{x['src']}" for x in soup.select('.img-mod-wrapper img')]
-        links = [f"{FETCH_URL}{x['href']}" for x in soup.select('.pic a')]
+        imgs = [f"{self.FETCH_URL}{x['src']}" for x in soup.select('.img-mod-wrapper img')]
+        links = [f"{self.FETCH_URL}{x['href']}" for x in soup.select('.pic a')]
         return titles, imgs, links
 
     def fetch(self):
@@ -41,33 +53,8 @@ class Food():
             pass
 
 
-# 早餐
-def getBreakfast():
-    return Food('http://tools.2345.com/meishi/zaocan.htm', 62)
-
-
-# 午餐
-def getLunch():
-    return Food('http://tools.2345.com/meishi/wucan.htm', 63)
-
-
-# 下午茶
-def getAfternoonTea():
-    return Food('http://tools.2345.com/meishi/xiawucha.htm', 66)
-
-
-# 晚餐
-def getDinner():
-    return Food('http://tools.2345.com/meishi/wancan.htm', 64)
-
-
-# 夜宵
-def getMidnightSnack():
-    return Food('http://tools.2345.com/meishi/xiaoye.htm', 65)
-
-
 if __name__ == '__main__':
-    f = getBreakfast()
+    f = Food2345(Food2345.FOOD_2345_WEBSITE_TYPE_DICT[0][0], Food2345.FOOD_2345_WEBSITE_TYPE_DICT[0][1])
     res = f.fetch()
     if res:
         for title, src in zip(res[0], res[2]):
